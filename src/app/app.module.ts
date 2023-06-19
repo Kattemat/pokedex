1d2cb0249c577c9fc68ce0c163735979e5a1540b
kmkarlse <77330597+kmkarlse@users.noreply.github.com>
2023-06-19 17:21:00 +0200

    layout start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokedexHeaderComponent } from './component/pokedex-header/pokedex-header.component';
+import { PokemonComponent } from './component/pokemon/pokemon.component';
+import { ShowcaseComponent } from './component/showcase/showcase.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PokedexHeaderComponent, PokemonComponent, ShowcaseComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
